21fc21c3495d98271390e5cc69c6a94e62ed0b11
Vinnie Ferreira <vinicius.ferreira@gmail.com>
2022-11-25 22:23:07 -0300

    Update nova.sh

--- nova.sh
+++ nova.sh
@@ -17,7 +17,7 @@ if [ $CPU != x86_64 ]; then
     exit
 fi
 
-
+#começando
 
 echo 'Seja bem vindo! Vamos deixar tudo pronto.'
 
@@ -27,28 +27,31 @@ sudo apt-get install gdebi
 cd
 cd Downloads
 
-echo Instalando o Safe Sign...
-
-wget https://github.com/Vinnie-Ferreira/assinadordigital-oab-debian/archive/refs/heads/main.zip
-sudo unzip main.zip
-cd assinadordigital-oab-debian-main
-sudo dpkg -i safesign_2_0_amd64.deb
-
-echo 'Agora, vamos instalar o pje-Office.'
+#Baixando arquivos
 
 wget https://cnj-pje-programs.s3-sa-east-1.amazonaws.com/pje-office/pje-office_amd64.deb
+--2022-11-25 22:08:30--  https://cnj-pje-programs.s3-sa-east-1.amazonaws.com/pje-office/pje-office_amd64.deb
+wget https://github.com/Vinnie-Ferreira/instaladoroabrj/releases/download/assinador/safesign_2_0_amd64.deb
+
+#instalando safesign
+
+sudo dpkg -i safesign_2_0_amd64.deb
+
+#instalando o pje-Office
+
 sudo dpkg -i pje-office_amd64.deb
 
 cd
-
 cd /usr/share/pje-office/
 chmod +x pjeOffice.sh
 ./pjeOffice.sh
 
 sudo apt-get update
 
-echo 'Agora tente logar no PJE do TJRJ.'
+rm safesign_2_0_amd64.deb
+rm pje-office_amd64.deb
 
+echo 'Instalações do SafeSign e PJE Office estão completas. Agora tente logar no PJE do TJRJ.'
 
 #zsh...
 
@@ -71,4 +74,3 @@ else
 echo Vc merece um terminal cafona
 exit
 fi
-
